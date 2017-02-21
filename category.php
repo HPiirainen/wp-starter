7ab92b502e3a77594eea2c9d77ee2d94e746037d
Hermanni Piirainen <hermanni.piirainen@floapps.com>
2017-02-21 14:59:24 +0200

    Add category template

---
+++ category.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying category posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ */
+
+get_header(); ?>
+
+	<div class="container-fluid">
+        <div class="row">
+            <?php get_sidebar(); ?>
+			
+			<h1 class=""><?php echo single_cat_title(); ?></h1>
+			
+			<?php
+			if ( have_posts() ) : ?>
+				
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
+					
+					the_title( '<h2 class="">', '</h2>' );
+					the_content();
+					
+				endwhile;
+
+				// TODO: check if needed and if this function works
+				the_posts_navigation();
+	
+			else :
+			
+				get_template_part( 'template-parts/content', 'none' );
+				
+			endif; ?>
+
+		</div>
+	</div>
+
+<?php get_footer(); ?>
