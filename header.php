37f176c2315d57f205c5cd89fec8db3a655d0542
Hermanni Piirainen <hermanni.piirainen@floapps.com>
2017-11-07 15:18:43 +0200

    Minor fix to meta tag

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" type="image/x-icon">
     <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" type="image/x-icon">
 	<?php wp_head(); ?>
