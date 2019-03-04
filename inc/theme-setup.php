d1ce75d474b23315b7514756e23785963207db6a
Hermanni Piirainen <hermanni.piirainen@floapps.com>
2019-03-04 15:22:06 +0200

    Function for font URL, add font to editor styles

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -18,7 +18,12 @@ function flo_starter_theme_setup() {
 
 	// Use editor stylesheet
 	add_theme_support( 'editor-styles' );
-	add_editor_style( 'editor-style.css' );
+	add_editor_style(
+		array(
+			flo_starter_get_font_url(),
+			'editor-style.css',
+		)
+	);
 
 	// Register menus
 	register_nav_menus(
@@ -33,6 +38,15 @@ function flo_starter_theme_setup() {
 add_action( 'after_setup_theme', 'flo_starter_theme_setup' );
 
 /**
+ * Return font URL
+ */
+function flo_starter_get_font_url() {
+	$protocol = is_ssl() ? 'https' : 'http';
+	$font_url = "$protocol://fonts.googleapis.com/css?family=Open+Sans:300,400,500,700";
+	return $font_url;
+}
+
+/**
  * Enqueue theme scripts and styles.
  */
 function flo_starter_scripts_styles() {
@@ -42,7 +56,8 @@ function flo_starter_scripts_styles() {
 	wp_enqueue_script( 'flo_starter-bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js', array( 'jquery' ), true, true );
 	wp_enqueue_script( 'flo_starter-mainjs',  get_theme_file_uri( "js/{$main_js_file_name}.js" ), array( 'jquery' ), filemtime( get_theme_file_path( "js/{$main_js_file_name}.js" ) ), true );
 
-	wp_enqueue_style( 'flo_starter-bootstrap-css', get_stylesheet_directory_uri() . '/css/bootstrap.css' );
+// 	wp_enqueue_style( 'flo_starter-fonts', esc_url_raw( flo_starter_get_font_url() ) );
+	wp_enqueue_style( 'flo_starter-bootstrap-css', get_theme_file_uri( 'css/bootstrap.css' ) );
 	wp_enqueue_style( 'flo_starter-style', get_theme_file_uri( "css/{$main_css_file_name}.css" ), array( 'flo_starter-bootstrap-css' ), filemtime( get_theme_file_path( "css/{$main_css_file_name}.css" ) ) );
 }
 add_action( 'wp_enqueue_scripts', 'flo_starter_scripts_styles' );
