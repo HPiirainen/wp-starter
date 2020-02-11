075059dc45462e721cddcee04ded1553ee7ed5e1
Hermanni Piirainen <hermanni.piirainen@floapps.com>
2020-02-11 17:01:09 +0200

    Remove font-size selection, add new theme supports

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -8,13 +8,21 @@ function flo_starter_after_setup_theme() {
 	// Theme supports
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+	add_theme_support( 'html5', [
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+		'script',
+		'style'
+	] );
 
 	// Gutenberg
 	add_theme_support( 'responsive-embeds' );
 	add_theme_support( 'editor-color-palette' );
 	add_theme_support( 'disable-custom-colors' );
-	add_theme_support( 'editor-font-sizes', [[]] );
+	add_theme_support( 'editor-font-sizes', [] );
 	add_theme_support( 'disable-custom-font-sizes' );
 
 	// Register menus
