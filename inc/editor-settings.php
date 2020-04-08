f32fe136d1e5dd175ae2d25076b549eefc7121a1
Hermanni Piirainen <hermanni.piirainen@floapps.com>
2020-04-08 10:00:07 +0300

    Disable Gutenberg default fullscreen mode (from WP 5.4)

--- inc/editor-settings.php
+++ inc/editor-settings.php
@@ -1,6 +1,22 @@
 <?php
 
 /**
+ * Disable Gutenberg default fullscreen mode starting from WP 5.4
+ */
+function flo_starter_disable_default_fullscreen_mode() {
+	$script = "
+		jQuery( window ).load(function() {
+			const isFullscreenMode = wp.data.select( 'core/edit-post' ).isFeatureActive( 'fullscreenMode' );
+			if ( isFullscreenMode ) {
+				wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'fullscreenMode' );
+			}
+		});
+	";
+	wp_add_inline_script( 'wp-blocks', $script );
+}
+add_action( 'enqueue_block_editor_assets', 'flo_starter_disable_default_fullscreen_mode' );
+
+/**
  * Set allowed blocks in Gutenberg editor
  *
  * Blocks not listed here are disallowed
